9392276a51873cc943cefe9931c655bd369cde48
guyzigdon <31125017+guyzigdon@users.noreply.github.com>
2026-02-25 11:47:20 +0200

    fix mirroring by default (#12)
    * fix mirroring by default
    
    * renaming
    
    * Renaming
    
    * simplify
    
    ---------
    
    Co-authored-by: Guy Zigdon <guyz@hlil-423-lap.qb.hailotech>

--- hailo_apps/python/pipeline_apps/drone_follow/drone_follow.py
+++ hailo_apps/python/pipeline_apps/drone_follow/drone_follow.py
@@ -13,6 +13,7 @@ import asyncio
 import logging
 import os
 import signal
+import sys
 import threading
 import time
 
@@ -416,8 +417,8 @@ def create_app(shared_state, target_state=None, eos_reached=None, ui_state=None,
                 frame_rate=self.frame_rate,
                 sync=self.sync,
                 input_codec=self.input_codec,
-                horizontal_mirror=not self.no_mirror,
-                vertical_mirror=self.v_mirror,
+                horizontal_mirror=not self.no_horizontal_mirror,
+                vertical_mirror=self.vertical_mirror,
             )
 
             nms_score_thresh = self.nms_score_threshold if self.nms_score_threshold is not None else 0.001
